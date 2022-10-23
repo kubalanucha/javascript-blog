55952a924cc18e63a3a13d20839ab9eee90d1737
Kuba Lanucha <kubalanucha@gmail.com>
2022-10-24 01:15:25 +0200

    Add generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -30,3 +30,56 @@ console.log(links);
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
+/*generateTitleLinks*/
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list',
+  optArticleAuthorSelector = '.post-author',
+  optAuthorRightList = '.sidebar .authors';
+
+function generateTitleLinks(customSelector = '') {
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  /* for each article */
+  const articles = document.querySelectorAll(
+    optArticleSelector + customSelector
+  );
+
+  let html = '';
+
+  for (let article of articles) {
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+
+    /* find the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML =
+      '<li><a href="#' +
+      articleId +
+      '"><span>' +
+      articleTitle +
+      '</span></a></li>';
+
+    /* insert link into titleList */
+
+    titleList.innerHTML = titleList.innerHTML + linkHTML;
+
+    /* insert link into HTML variable */
+
+    html = html + linkHTML;
+  }
+  console.log(titleList);
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll('.titles a');
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
+}
+
+generateTitleLinks();
